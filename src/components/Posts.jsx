4c16ba262f332c20ce2d7358825d667a0bab3c25
Ben Bryant <brbryant2639@gmail.com>
2020-03-17 11:16:20 -0500

    fixed props reference to properly display fetched content

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Posts = (user, posts) => {
-    console.log(user.id);
-    console.log(posts.arr);
+const Posts = (user) => {
+    const [posts, setPosts] = useState([]);
+
+    const getPosts = async () => {
+        let res = await fetch('https://jsonplaceholder.typicode.com/posts')
+        let posts = await res.json();
+        setPosts(posts);
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, [])
+
     return (
-        <React.Fragment>
-            
-            )}
-        </React.Fragment>
+        <div>
+            {posts.map(post => {
+                if (user.id === post.userId) {
+                    return (
+                        <div key={post.id} className='p-3'>
+                            <hr></hr>
+                            <h6>{post.title}</h6>
+                            <p>{post.body}</p>
+                        </div>
+                    )
+                } else {
+                    return null;
+                }
+            })}
+        </div>
     )
 }
 
