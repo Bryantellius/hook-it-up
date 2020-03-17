6fb9b6cf1d65fc4ffa0d87f7f96f4114cde407b0
Ben Bryant <brbryant2639@gmail.com>
2020-03-17 11:12:51 -0500

    home component returns logo

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <img src="ColloquiumLogo.png" alt="Site Logo of Sun and Moon" className="img-thumbnail bg-warning"/>
+    )
+}
+
+export default Home;
